40cd52cfa07fe966feb31149348daeec2f3ef0b9
Andrea Burattin <andrea.burattin@gmail.com>
2021-10-12 09:24:42 +0200

    Added global status icon

--- src/components/widgets/SystemStatus.vue
+++ src/components/widgets/SystemStatus.vue
@@ -1,6 +1,7 @@
 <template>
   <footer class="mb-1 mx-3 small" style="position: fixed; bottom: 0">
     <p>
+      <font-awesome-icon icon="circle" v-bind:class="this.systemStatusClass" />
       {{ this.getSystemStatus() }}<br />
       <small class="text-secondary">
         <font-awesome-icon icon="server" class="mr-1" v-bind:class="this.systemStatus" /> {{ this.$backend.getHostname() }} <br />
@@ -14,6 +15,17 @@
 export default {
   name: 'SystemStatus',
   props: ['systemStatus', 'systemStatusMiner'],
+  computed: {
+    systemStatusClass: function() {
+      if (this.systemStatus === 'online' && this.systemStatusMiner === 'online') {
+        return 'online'
+      } else if (this.systemStatus === 'booting' || this.systemStatusMiner === 'booting') {
+        return 'booting'
+      } else {
+        return 'offline'
+      }
+    },
+  },
   methods: {
     getSystemStatus () {
       if (this.systemStatus === 'online' && this.systemStatusMiner === 'online') {
